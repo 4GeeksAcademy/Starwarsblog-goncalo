2b14afce3ee5681e494ce3a376b31f6da4567521
Gmarques023 <153223582+Gmarques023@users.noreply.github.com>
2024-05-29 00:16:43 +0000

    create get characters function

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,24 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useContext} from "react";
 import "../../styles/home.css";
+import {Context} from "../store/appContext"
+import {Link} from "react-router-dom"
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+
+	const {store, actions} = useContext(Context)
+	console.log(store.character)
+
+	return (
+		<div className="text-center mt-5">
+			{store.character && store.character.map(el => (
+				<div key={el.uid} className="card">
+					<h2>{el.name}</h2>
+					<img src={`https://starwars-visualguide.com/assets/img/characters/${el.uid}.jpg`}/>
+					<Link className="btn btn-primary" to={`/single/${el.uid}`}>
+						Learn More
+					</Link>
+				</div>
+			))}
+		</div>
+	)
+};
